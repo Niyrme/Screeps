feb55950c98373433cdaa0cfc40967d740f2d63b
evelyn <niyrme@gmail.com>
2024-08-01 22:24:29 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-// include all utils
-import "./util";
-// include all prototypes
-import "./prototypes";
-import ErrorMapper from "./util/errorMapper";
+import "./Global";
+import "./Lib";
+import "./Prototypes";
 
-export const loop = ErrorMapper(function () {
-	for (const name in Memory.creeps) {
-		if (!(name in Game.creeps)) {
-			delete Memory.creeps[name];
+import profiler from "screeps-profiler";
+
+profiler.enable();
+
+export const loop = profiler.wrap(
+	function () {
+		for (const name in Memory.creeps) {
+			if (!(name in Game.creeps)) {
+				delete Memory.creeps[name];
+			}
 		}
-	}
-});
+	},
+);
